72cb73c07e993f1f7c15439c435191b1efd39363
GenerativeAI-openai <ijunu3343@gmail.com>
2025-04-27 12:28:45 +0900

    Add files via upload

---
+++ img/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzWZtDMzb4cNoauLA4NvmxqO6C_ot00AA",
+  authDomain: "smssend-b82b3.firebaseapp.com",
+  databaseURL: "https://smssend-b82b3-default-rtdb.firebaseio.com",
+  projectId: "smssend-b82b3",
+  storageBucket: "smssend-b82b3.firebasestorage.app",
+  messagingSenderId: "734888172114",
+  appId: "1:734888172114:web:0b2c99fee212f6487c1d8b",
+  measurementId: "G-J3VJPW92L2"
+};
+
+export default firebaseConfig;
+//firebase-config
